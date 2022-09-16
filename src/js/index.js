26f4ecc229e1f789e9648763be128cb73ff7b74e
Filip Kocian <filkocian@gmail.com>
2022-09-16 14:33:29 +0200

    create leaflet map

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,10 @@
+import * as L from 'leaflet';
 
+const map = L.map('map', {
+  zoomControl: false,
+}).setView([51.505, -0.09], 13);
+
+L.tileLayer('https://{s}.tile.openstreetmap.fr/hot//{z}/{x}/{y}.png', {
+  attribution:
+    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+}).addTo(map);
