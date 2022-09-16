7ebc0dba8542d220f7428675a44c8ee8ab2d5cbb
Filip Kocian <filkocian@gmail.com>
2022-09-16 22:06:34 +0200

    create menu UI component

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,4 @@
+// Map creation
 import * as L from 'leaflet';
 
 const map = L.map('map', {
@@ -8,3 +9,26 @@ L.tileLayer('https://{s}.tile.openstreetmap.fr/hot//{z}/{x}/{y}.png', {
   attribution:
     '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
 }).addTo(map);
+
+// Closing and opening menu
+const btnMenuOpen = document.getElementById('open-menu');
+const btnMenuClose = document.getElementById('close-menu');
+const menu = document.getElementById('menu');
+const uploadBtn = document.getElementById('upload');
+
+btnMenuOpen.addEventListener('click', () => {
+  if (menu.classList.contains('active')) return;
+
+  menu.classList.add('active');
+  uploadBtn.classList.add('move');
+});
+
+btnMenuClose.addEventListener('click', () => {
+  if (!menu.classList.contains('active')) return;
+
+  menu.classList.remove('active');
+  uploadBtn.classList.remove('move');
+  uploadBtn.classList.add('transitioning');
+
+  setTimeout(() => uploadBtn.classList.remove('transitioning'), 300);
+});
