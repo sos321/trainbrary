05710351195e16c4bf322ac8c83438fe11968dc5
Filip Kocian <filkocian@gmail.com>
2022-12-29 15:26:34 +0100

    delete config.js

--- src/js/config.js
+++
@@ -1,4 +0,0 @@
-export const TIMEOUT_SEC = 10;
-export const INITIAL_ZOOM = 13;
-export const A_API =
-  'AAPK56c5fb24ae10420f805daa396e1fb8e9FpnaxUC1LY9RO1ZdbzJ4Cgml0uwn5ZnMhbHACXqpoI36ZPxzGSgwM-EUrzaf4HAC';
